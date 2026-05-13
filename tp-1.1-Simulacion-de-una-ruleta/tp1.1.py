b394658ff7f18be6218834c8df04760628652b75
Santiago Malet <santiago.malet.12@gmail.com>
2026-05-13 08:06:28 -0300

    Hago graficas de una sola corrida y pongo n = 30 por TCL

--- tp-1.1-Simulacion-de-una-ruleta/tp1.1.py
+++ tp-1.1-Simulacion-de-una-ruleta/tp1.1.py
@@ -1,21 +1,4 @@
-# ASI COMO ESTA LAS TIRADAS SE CREAN POR CADA FUNCION, LO QUE HACE QUE LOS GRAFICOS NO SEAN COMPARABLES ENTRE SI Y QUE SEA
-# MAS COSTOSO EN TERMINOS DE RENDIMIENTO, SOLO HABRIA QUE CREAR UNA FUNCION PARA SIMULAR LAS TIRADAS Y LISTO, ES FACIL PERO ME DA PAJA
-# DESPUES LO HAGO
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# BORRAR ESTO PARA ENVIARLO
+
 
 """
 Algunas anotaciones del experimento (las pongo por si hay que agregarlas al informe en latex despues las sacamos de aca):
@@ -37,7 +20,7 @@ import statistics
 import matplotlib.pyplot as plt
 
 # cantidad de tiradas
-n = 3700
+n = 30
 
 # cantidad de corridas
 c = 4
@@ -61,7 +44,7 @@ def tiradas():
     return corridas
 
 
-def frecuencia_relativa(listavalores):
+def frecuencia_relativa(listavalores,c):
     fr_c = []
     for i in range(c):
         exitos = 0
@@ -74,7 +57,7 @@ def frecuencia_relativa(listavalores):
     return fr_c
 
 
-def promedio_observado(listavalores):
+def promedio_observado(listavalores, c):
 
     vp_c = []
     for i in range(c):
@@ -85,7 +68,7 @@ def promedio_observado(listavalores):
     return vp_c
 
 
-def calcular_varianza(listavalores):
+def calcular_varianza(listavalores, c):
 
     vv_c = []
     for i in range(c):
@@ -98,7 +81,7 @@ def calcular_varianza(listavalores):
     return vv_c
 
 
-def calcular_desvioestandar(listavalores):
+def calcular_desvioestandar(listavalores, c):
     dv_c = []
     for i in range(c):
         dv_t = []
@@ -148,8 +131,85 @@ def frecuencia_absoluta():
 # Generacion de todos los valores de las tiradas de todas las corridas
 listavalores = tiradas()
 
+
+#
+#
+#A partir de aca se generan los graficos con una sola corrida
+#
+#
+
+frecuenciarelativa = frecuencia_relativa(listavalores, 1)
+
+plt.figure(figsize=(10, 4))
+plt.plot(
+    range(1, n + 1),
+    frecuenciarelativa[0],
+    label="Corrida 1",
+    alpha=0.7
+)
+plt.axhline(y=1 / 37, color="blue", linestyle="-", label="fr esperada (1/37 ≈ 0.02703)")
+plt.title("Frecuencia relativa acumulada respecto al número de tiradas")
+plt.xlabel("Número de tiradas")
+plt.ylabel("Frecuencia relativa")
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+
+promedioobservado = promedio_observado(listavalores,1)
+
+plt.figure(figsize=(10, 4))
+
+plt.plot(range(1, n + 1), promedioobservado[0], label="Corrida 1", alpha=0.7)
+plt.axhline(y=18, color="blue", linestyle="-", label="VP Esperado (18)")
+plt.title("Valor Promedio Acumulado respecto al número de tiradas")
+plt.xlabel("Número de tiradas")
+plt.ylabel("Valor Promedio")
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+
+varianza = calcular_varianza(listavalores,4)
+
+plt.figure(figsize=(10, 4))
+
+plt.plot(range(1, n + 1), varianza[0], label="Corrida 1", alpha=0.7)
+plt.axhline(y=114.67, color="blue", linestyle="-", label="Varianza Esperada (114.67)")
+plt.title("Varianza Acumulada respecto al número de tiradas")
+plt.xlabel("Número de tiradas")
+plt.ylabel("Varianza")
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+desvioestandar = calcular_desvioestandar(listavalores,1)
+
+plt.figure(figsize=(10, 4))
+
+plt.plot(range(1, n + 1), desvioestandar[0], label="Corrida 1", alpha=0.7)
+plt.axhline(y=10.68, color="blue", linestyle="-", label="Desvío Esperado (10.68)")
+plt.title("Desvío Estándar Acumulado respecto al número de tiradas")
+plt.xlabel("Número de tiradas")
+plt.ylabel("Desvío Estandar")
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+
+#
+#
+#A partir de aca se generan los graficos con varias tiradas
+#
+#
+
 # Calculo frecuencias relativas y genero grafico
-frecuenciarelativa = frecuencia_relativa(listavalores)
+frecuenciarelativa = frecuencia_relativa(listavalores, c)
 
 plt.figure(figsize=(10, 4))
 for i in range(c):
@@ -168,7 +228,7 @@ plt.tight_layout()
 plt.show()
 
 # calculo valores promedios y genero grafico
-promedioobservado = promedio_observado(listavalores)
+promedioobservado = promedio_observado(listavalores,4)
 
 plt.figure(figsize=(10, 4))
 for i in range(c):
@@ -182,7 +242,7 @@ plt.tight_layout()
 plt.show()
 
 # Calculo varianzas y genero grafico
-varianza = calcular_varianza(listavalores)
+varianza = calcular_varianza(listavalores,4)
 
 plt.figure(figsize=(10, 4))
 for i in range(c):
@@ -196,7 +256,7 @@ plt.tight_layout()
 plt.show()
 
 # Calculo el desvio y genero grafico
-desvioestandar = calcular_desvioestandar(listavalores)
+desvioestandar = calcular_desvioestandar(listavalores,4)
 
 plt.figure(figsize=(10, 4))
 for i in range(c):
@@ -210,112 +270,3 @@ plt.tight_layout()
 plt.show()
 
 
-frecuencia_absoluta()
-
-
-"""
-#simulo la ruleta en 3700 tiradas 
-for i in range(1, n + 1):
-    valor = random.randint(0, 36)
-    listavalores.append(valor)
-    
-    
-    if valor == numero_elegido:
-        exitos += 1
-    
-    #sumo a la lista de frecuencia relativa
-    fr_n.append(exitos / i)
-    
-    #sumo a la lista de valor promedio
-    vp_n.append(sum(listavalores) / i)
-
-    
-    if i > 1:  
-        dv.append(statistics.stdev(listavalores))
-    else:
-        dv.append(0)  
-
-    
-    if i > 1:  
-        vv.append(statistics.variance(listavalores))
-    
-
-frecuencias = [0] * 37 
-
-for valor in listavalores:
-    frecuencias[valor] += 1
-
-
-
-
-plt.figure(figsize=(10, 4))
-plt.plot(fr_n, label='Frecuencia Relativa', color='red')
-plt.axhline(y=1/37, color='blue', linestyle='-', label='Esperada (1/37)')
-plt.title('Número de tiradas')
-plt.ylabel('Frecuencia relativa')
-plt.legend()
-plt.tight_layout()
-plt.show()
-
-
-
-plt.figure(figsize=(10, 4))
-plt.plot(vp_n, label='Promedio Observado', color='red')
-plt.axhline(y=18, color='blue', linestyle='-', label='VP Esperado (18)')
-plt.title('Número de tiradas')
-plt.ylabel('Valor Promedio')
-plt.legend()
-plt.tight_layout()
-plt.show()
-
-
-
-"""
-
-
-"""
-plt.figure(figsize=(10, 4))
-plt.plot(dv, label='Desvío Observado', color='red')
-plt.axhline(y=10.68, color='blue', linestyle='-', label='Desvío Esperado (10.68)')
-plt.title('Número de tiradas')
-plt.ylabel('Desvío Estandar')
-plt.legend()
-plt.tight_layout()
-plt.show()
-
-plt.figure(figsize=(10, 4))
-plt.plot(vv, label='Varianza Observada', color='red')
-plt.axhline(y=114.67, color='blue', linestyle='-', label='Varianza Esperada (114.67)')
-plt.title('Número de tiradas')
-plt.ylabel('Varianza')
-plt.legend()
-plt.tight_layout()
-plt.show()
-
-plt.figure(figsize=(10, 4))
-plt.bar(range(37), frecuencias, color='red')
-plt.title('Frecuencia Absoluta de cada número')
-plt.xlabel('Número')
-plt.ylabel('Frecuencia Absoluta')
-plt.xticks(range(37))
-plt.tight_layout()
-plt.show()
-"""
-
-
-"""   
-print(listavalores)    
-moda = statistics.mode(listavalores)
-mediana = statistics.median(listavalores)
-desvio = statistics.stdev(listavalores)
-mean = statistics.mean(listavalores)
-varianza = statistics.variance(listavalores)
-desvio_poblacional = statistics.pstdev(listavalores)
-
-print("La moda es: ", moda)
-print("La media es: ", mediana)
-print("El desvio estandar es: ", desvio)
-print("La media es: ", mean)
-print("La varianza es: ", varianza)
-print("El desvio poblacional es: ", desvio_poblacional)
- """
